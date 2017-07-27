6ddff38d9383220439873632f211e763f01e5115
rafaellopes21 <rafaelsilva.1@univille.com>
2017-07-27 19:09:52 -0300

    commit

--- queue/src/queue/Q4.java
+++ queue/src/queue/Q4.java
@@ -2,5 +2,14 @@ package queue;
 //4- Dada um número inteiro n, retorne uma fila contendo todos os números maiores que zero e menores que n.
 public class Q4 {
 
+	public Queue<Integer> q4(int n){
+		Queue<Integer> queue = new ArrayQueue<>();
+		
+		for (int i = 1; i < n; i++) {
+			queue.enqueue(i);
+		}
+		
+		return queue;
+	}
 	
 }
